0d2c3ba74d7e68515556f6d86aa84064f2b54a3c
Dayane <dayanebmartins@gmail.com>
2024-04-14 19:20:14 -0300

    feat(Products): Defini o relacionamento de product com pack

--- backend/app/models/product.ts
+++ backend/app/models/product.ts
@@ -1,5 +1,7 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, manyToMany } from '@adonisjs/lucid/orm'
+import Pack from './pack.js'
+import type { ManyToMany } from '@adonisjs/lucid/types/relations'
 
 export default class Product extends BaseModel {
   @column({ isPrimary: true })
@@ -13,6 +15,9 @@ export default class Product extends BaseModel {
 
   @column()
   declare sales_price: number
+
+  @manyToMany(() => Pack)
+  declare pack: ManyToMany<typeof Pack>
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
