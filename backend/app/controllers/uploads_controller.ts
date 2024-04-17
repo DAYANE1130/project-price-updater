01f6dbba004056b049f19d5e48720ca769248de4
Dayane <dayanebmartins@gmail.com>
2024-04-16 22:22:48 -0300

    refactor(Upload): Refatora função  store separando a função que é responsavel pela leitura dop arquivo .csv

--- backend/app/controllers/uploads_controller.ts
+++ backend/app/controllers/uploads_controller.ts
@@ -3,10 +3,11 @@ import { cuid } from '@adonisjs/core/helpers'
 import app from '@adonisjs/core/services/app'
 import { MultipartFile } from '@adonisjs/core/bodyparser'
 import * as fs from 'node:fs'
-//import * as path from 'node:path'
 import * as csv from 'fast-csv'
+import CSVValidationService from '#services/csv_validation_service'
 
 export default class UploadsController {
+  // private csvValidationService = new CSVValidationService()
   /**
    * Display a list of resource
    */
@@ -20,6 +21,62 @@ export default class UploadsController {
   /**
    * Handle form submission for the create action
    */
+
+  // async processCsv(file: MultipartFile | null) {
+  //   return new Promise((resolve, reject) => {
+  //     const validationResults: any = []
+  //     //@ts-ignore
+  //     fs.createReadStream('./uploads/' + file.fileName)
+  //       .pipe(csv.parse({ headers: true }))
+  //       .on('data', async (row) => {
+  //         row.product_code = Number.parseInt(row.product_code, 10)
+  //         row.new_price = Number.parseFloat(row.new_price)
+  //         await updateProductValidator.validate(row)
+  //         const validationResult = await this.csvValidationService.validate(row)
+  //         console.log('first', validationResult)
+  //         validationResults.push(validationResult)
+  //         console.log('eu sou o array', validationResults)
+  //       })
+  //       .on('end', () => {
+  //         console.log('CSV file successfully processed')
+  //         resolve(validationResults)
+  //       })
+  //       .on('error', (error) => {
+  //         console.log('An error occurred:', error)
+  //         reject(error)
+  //       })
+  //   })
+  // }
+
+  async processCsv(file: MultipartFile | null) {
+    return new Promise((resolve, reject) => {
+      const validationPromises: any = []
+      //@ts-ignore
+      fs.createReadStream('./uploads/' + file.fileName)
+        .pipe(csv.parse({ headers: true }))
+        .on('data', (row) => {
+          row.product_code = Number.parseInt(row.product_code, 10)
+          row.new_price = Number.parseFloat(row.new_price)
+          // const validationResultPromise = updateProductValidator
+          //   .validate(row)
+          //   .then(() => CSVValidationService.validate(row))
+          validationPromises.push(row)
+        })
+        .on('end', () => {
+          Promise.all(validationPromises)
+            .then((validationResults) => {
+              console.log('CSV file successfully processed')
+              resolve(validationResults)
+            })
+            .catch(reject)
+        })
+        .on('error', (error) => {
+          console.log('An error occurred:', error)
+          reject(error)
+        })
+    })
+  }
+
   async store({ request, response }: HttpContext) {
     const arquivo: MultipartFile | null = request.file('meuArquivo', {
       size: '2mb',
@@ -36,12 +93,10 @@ export default class UploadsController {
       //@ts-ignore
       name: `${cuid()}.${arquivo.extname}`,
     })
-    //@ts-ignore
-    fs.createReadStream('./uploads/' + arquivo.fileName)
-      .pipe(csv.parse({ headers: true })) // primeira linha são os cabeçalhos
-      .on('error', (error) => console.error(error)) // caso algum erro ocorra
-      .on('data', (row) => console.log(row)) // convertetendo cada linha em um objeto js
-      .on('end', (rowCount: number) => console.log(`Parsed ${rowCount} rows`)) // analise concluida e informa numero de linhas analisadas
+    const arrayOfReadCsv = await this.processCsv(arquivo)
+    await CSVValidationService.validateProductFields(arrayOfReadCsv)
+    await CSVValidationService.validate(arrayOfReadCsv)
+    return arrayOfReadCsv
   }
   /**
    * Show individual record
