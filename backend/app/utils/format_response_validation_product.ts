a4116bf58b649696523754d4d32d3d88acb0507c
Dayane <dayanebmartins@gmail.com>
2024-04-19 16:23:40 -0300

    feat(Backend-Products):

--- backend/app/utils/format_response_validation_product.ts
+++ backend/app/utils/format_response_validation_product.ts
@@ -1,12 +1,29 @@
 import Product from '#models/product'
+import { ProductSendByUser } from '../Interfaces/product_types.js'
 
 export class FormatResponseValidationProduct {
-  static simplifiedResults(product: Product, newPrice: number) {
+  static simplifiedResults(products: any, data: ProductSendByUser[]) {
+    // if (!products || products.existingProducts) {
+    //   return [] // ou você pode retornar algum tipo de mensagem de erro aqui
+    // }
+    const formatResponseProducts = products.map((product: Product, index: number) => {
+      let priceCurrent = Number.parseFloat(product.$attributes.sales_price)
+      return {
+        code: product.$attributes.code,
+        name: product.$attributes.name,
+        current_price: Number(priceCurrent.toFixed(2)),
+        new_price: data[index].new_price,
+      }
+    })
+    return formatResponseProducts
+  }
+
+  static simplifiedResultError(product: Product, data: ProductSendByUser[], index: number) {
     return {
       code: product.$attributes.code,
       name: product.$attributes.name,
-      currentPrice: Number(product.$attributes.cost_price), // ou sales_price
-      newPrice: newPrice,
+      current_price: Number(product.$attributes.sales_price),
+      new_price: data[index].new_price,
     }
   }
 }
